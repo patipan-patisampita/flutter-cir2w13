3030d2d33b1345dc16aee2a1dc6768c9700e3a79
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-31 11:02:21 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -13,67 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Navigator',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.indigo),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter Navigator"),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text("Navigator"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print("Text");
-          },
-          child: Icon(Icons.home),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(
-                child: Center(
-                  child: Text(
-                    "HEADER",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
-                decoration: BoxDecoration(color: Colors.indigo),
-              ),
-              ListTile(
-                onTap: () {
-                  print("Home");
-                },
-                leading: Icon(Icons.home,color: Colors.indigo),
-                title: Text("Home",style: TextStyle(color: Colors.indigo)),
-              ),
-              ListTile(
-                onTap: () {
-                  print("MENU-2");
-                },
-                leading: Icon(Icons.account_circle,color: Colors.deepOrange),
-                title: Text("MENU-2"),
-              ),
-              ListTile(
-                leading: Icon(Icons.dns,color: Colors.deepOrange),
-                title: Text("MENU-3"),
-              ),
-              ListTile(
-                leading: Icon(Icons.api,color: Colors.deepOrange),
-                title: Text("MENU-4"),
-              ),
-              SizedBox(height: 5),
-              ListTile(
-                leading: Icon(Icons.power_settings_new,color: Colors.deepOrange),
-                title: Text("EXIT Program"),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: AppView(),
     );
   }
 }
@@ -83,6 +23,66 @@ class AppView extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Navigator"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("Navigator"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("Text");
+        },
+        child: Icon(Icons.home),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Center(
+                child: Text(
+                  "HEADER",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(color: Colors.indigo),
+            ),
+            ListTile(
+              onTap: () {
+                print("Home");
+              },
+              leading: Icon(Icons.home,color: Colors.indigo),
+              title: Text("Home",style: TextStyle(color: Colors.indigo)),
+            ),
+            ListTile(
+              onTap: () {
+                print("MENU-2");
+              },
+              leading: Icon(Icons.account_circle,color: Colors.deepOrange),
+              title: Text("MENU-2"),
+            ),
+            ListTile(
+              leading: Icon(Icons.dns,color: Colors.deepOrange),
+              title: Text("MENU-3"),
+            ),
+            ListTile(
+              leading: Icon(Icons.api,color: Colors.deepOrange),
+              title: Text("MENU-4"),
+            ),
+            SizedBox(height: 5),
+            ListTile(
+              leading: Icon(Icons.power_settings_new,color: Colors.deepOrange),
+              title: Text("EXIT Program"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
