a73a9c8c8c9d8d535b27d7b50e2a9df4025c4352
patipan-patisampita <patipan.patisampita@gmail.com>
2022-01-31 10:57:08 +0700

    update

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Navigator',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Navigator"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text("Navigator"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("Text");
+          },
+          child: Icon(Icons.home),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Center(
+                  child: Text(
+                    "HEADER",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                decoration: BoxDecoration(color: Colors.indigo),
+              ),
+              ListTile(
+                onTap: () {
+                  print("Home");
+                },
+                leading: Icon(Icons.home,color: Colors.indigo),
+                title: Text("Home",style: TextStyle(color: Colors.indigo)),
+              ),
+              ListTile(
+                onTap: () {
+                  print("MENU-2");
+                },
+                leading: Icon(Icons.account_circle,color: Colors.deepOrange),
+                title: Text("MENU-2"),
+              ),
+              ListTile(
+                leading: Icon(Icons.dns,color: Colors.deepOrange),
+                title: Text("MENU-3"),
+              ),
+              ListTile(
+                leading: Icon(Icons.api,color: Colors.deepOrange),
+                title: Text("MENU-4"),
+              ),
+              SizedBox(height: 5),
+              ListTile(
+                leading: Icon(Icons.power_settings_new,color: Colors.deepOrange),
+                title: Text("EXIT Program"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
